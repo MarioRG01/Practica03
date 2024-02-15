2d4f6db09bb9abe9f69ed1ec4e45664d5a73b6d2
MarioRG01 <mickey200123@gmail.com>
2024-02-15 09:57:46 -0600

    Peliculas

---
+++ app/src/main/java/com/example/practica03/Login.kt
@@ -0,0 +1,57 @@
+package com.example.practica03
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
+
+class Login : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth;
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        auth = Firebase.auth
+
+        var btnLogin = findViewById<Button>(R.id.Login)
+
+        btnLogin.setOnClickListener {
+            auth.signInWithEmailAndPassword("mario@mario.com","123456").addOnCompleteListener{
+                task ->
+
+                if (task.isSuccessful)
+                {
+                    Toast.makeText(this,"Se inicio sesion correctamente", Toast.LENGTH_LONG).show()
+                }
+                else{
+                    Toast.makeText(this,"Error"+task.exception!!.message.toString(), Toast.LENGTH_LONG).show()
+                }
+            }
+
+        }
+    }
+
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        if (currentUser!= null)
+        {
+            Toast.makeText(this,"No hay usuarios autenticados", Toast.LENGTH_LONG).show()
+
+        }
+        else
+        {
+            Toast.makeText(this,"Ya estas autenticado", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    public override fun onDestroy() {
+        super.onDestroy()
+        auth.signOut()
+    }
+}
